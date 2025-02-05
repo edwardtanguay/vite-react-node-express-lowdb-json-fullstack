288c1c1633995bb1f3908649e4bf87edb10c081a
Edward Tanguay (windows) <edward@tanguay.info>
2025-02-05 23:58:12 +0100

    serves html

--- backend/api/server.ts
+++ backend/api/server.ts
@@ -1,13 +1,24 @@
-import express from 'express';
-import { flashcardRouter } from './routers/flashcardRouter';
+import express from "express";
+import { flashcardRouter } from "./routers/flashcardRouter";
 
 export const app = express();
 app.use(express.json());
 
-app.get('/', (_req, res) => {
-	res.json({
-		appName: "API for AppLearn version 0.2"
-	})
+app.get("/", (_req, res) => {
+	res.send(`
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Virtual Medical Site Backend</title>
+    </head>
+    <body>
+        <h1>Welcome to the Virtual Medical Site backend</h1>
+        <p>Version: <strong>1.0</strong></p>
+    </body>
+    </html>
+  `);
 });
 
-app.use('/api/flashcards', flashcardRouter);
+app.use("/api/flashcards", flashcardRouter);
