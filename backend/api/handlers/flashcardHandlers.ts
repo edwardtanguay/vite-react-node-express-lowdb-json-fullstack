0aa48737c44bcf5857505553a958e0425947ad72
Edward Tanguay (windows) <edward@tanguay.info>
2025-02-06 11:04:39 +0100

    name changed

--- backend/api/handlers/flashcardHandlers.ts
+++ backend/api/handlers/flashcardHandlers.ts
@@ -1,41 +1,51 @@
-import { join } from 'path';
-import { JSONFile } from 'lowdb/node';
-import { Low } from 'lowdb';
-import { IDatabase, IFlashcard, INewFlashcard, IPatchFlashcard } from '../../interfaces';
-import * as tools from '../tools';
+import { join } from "path";
+import { JSONFile } from "lowdb/node";
+import { Low } from "lowdb";
+import {
+	IDatabase,
+	IFlashcard,
+	INewFlashcard,
+	IPatchFlashcard,
+} from "../../types";
+import * as tools from "../tools";
 
 const projectBasePath = process.cwd();
-const dbPathAndFileName = join(projectBasePath, 'backend/data/db.json');
+const dbPathAndFileName = join(projectBasePath, "backend/data/db.json");
 const adapter = new JSONFile<IDatabase>(dbPathAndFileName);
 const db: Low<IDatabase> = new Low<IDatabase>(adapter, {} as IDatabase);
 await db.read();
 
 export const getAllFlashcards = () => {
 	return db.data.flashcards;
-}
+};
 
 export const getOneFlashcard = (suuid: string) => {
-	const flashcard = db.data.flashcards.find(m => m.suuid === suuid);
+	const flashcard = db.data.flashcards.find((m) => m.suuid === suuid);
 	if (flashcard) {
 		return flashcard;
 	} else {
 		return null;
 	}
-}
+};
 
 export const createFlashcard = async (newFlashcard: INewFlashcard) => {
 	const flashcard: IFlashcard = {
 		...newFlashcard,
-		suuid: tools.generateSuuid()
-	}
+		suuid: tools.generateSuuid(),
+	};
 	const flashcards = db.data.flashcards;
 	flashcards.push(flashcard);
 	await db.write();
 	return flashcard;
-}
+};
 
-export const replaceFlashcard = async (suuid: string, newFlashcard: INewFlashcard) => {
-	const flashcard: IFlashcard | undefined = db.data.flashcards.find(m => m.suuid === suuid);
+export const replaceFlashcard = async (
+	suuid: string,
+	newFlashcard: INewFlashcard
+) => {
+	const flashcard: IFlashcard | undefined = db.data.flashcards.find(
+		(m) => m.suuid === suuid
+	);
 	if (flashcard) {
 		flashcard.category = newFlashcard.category;
 		flashcard.front = newFlashcard.front;
@@ -45,12 +55,18 @@ export const replaceFlashcard = async (suuid: string, newFlashcard: INewFlashcar
 	} else {
 		return null;
 	}
-}
+};
 
-export const replaceSomeFieldsInFlashcard = async (suuid: string, patchFlashcard: IPatchFlashcard) => {
-	const flashcard: IFlashcard | undefined = db.data.flashcards.find(m => m.suuid === suuid);
+export const replaceSomeFieldsInFlashcard = async (
+	suuid: string,
+	patchFlashcard: IPatchFlashcard
+) => {
+	const flashcard: IFlashcard | undefined = db.data.flashcards.find(
+		(m) => m.suuid === suuid
+	);
 	if (flashcard) {
-		if (patchFlashcard.category) flashcard.category = patchFlashcard.category;
+		if (patchFlashcard.category)
+			flashcard.category = patchFlashcard.category;
 		if (patchFlashcard.front) flashcard.front = patchFlashcard.front;
 		if (patchFlashcard.back) flashcard.back = patchFlashcard.back;
 		await db.write();
@@ -58,12 +74,14 @@ export const replaceSomeFieldsInFlashcard = async (suuid: string, patchFlashcard
 	} else {
 		return null;
 	}
-}
+};
 
 export const deleteFlashcard = async (suuid: string) => {
-	const deletedFlashcard = db.data.flashcards.find(m => m.suuid === suuid);
+	const deletedFlashcard = db.data.flashcards.find((m) => m.suuid === suuid);
 	if (deletedFlashcard) {
-		const indexToRemove = db.data.flashcards.findIndex((flashcard) => flashcard.suuid === suuid);
+		const indexToRemove = db.data.flashcards.findIndex(
+			(flashcard) => flashcard.suuid === suuid
+		);
 		if (indexToRemove !== -1) {
 			db.data.flashcards.splice(indexToRemove, 1);
 			await db.write();
@@ -72,4 +90,4 @@ export const deleteFlashcard = async (suuid: string) => {
 	} else {
 		return null;
 	}
-}
+};
--- backend/api/routers/flashcardRouter.ts
+++ backend/api/routers/flashcardRouter.ts
@@ -1,65 +1,74 @@
-import { Router } from 'express';
-import * as flashcardHandler from '../handlers/flashcardHandlers';
-import { IFlashcard, INewFlashcard, IPatchFlashcard } from '../../interfaces';
+import { Router } from "express";
+import * as flashcardHandler from "../handlers/flashcardHandlers";
+import { IFlashcard, INewFlashcard, IPatchFlashcard } from "../../types";
 
 export const flashcardRouter = Router();
 
-flashcardRouter.get('/', (_req, res) => {
+flashcardRouter.get("/", (_req, res) => {
 	res.json(flashcardHandler.getAllFlashcards());
 });
 
-flashcardRouter.get('/:suuid', (req, res) => {
+flashcardRouter.get("/:suuid", (req, res) => {
 	const suuid = req.params.suuid;
-	const flashcard: IFlashcard | null = flashcardHandler.getOneFlashcard(suuid);
+	const flashcard: IFlashcard | null =
+		flashcardHandler.getOneFlashcard(suuid);
 	if (flashcard) {
 		res.json(flashcard);
 	} else {
 		res.status(404).json({
-			message: `Flashcard with suuid "${suuid}" was not found.`
-		})
+			message: `Flashcard with suuid "${suuid}" was not found.`,
+		});
 	}
 });
 
-flashcardRouter.post('/', async (req, res) => {
+flashcardRouter.post("/", async (req, res) => {
 	const newFlashcard: INewFlashcard = req.body;
-	const createdFlashcard = await flashcardHandler.createFlashcard(newFlashcard);
+	const createdFlashcard =
+		await flashcardHandler.createFlashcard(newFlashcard);
 	res.json(createdFlashcard);
 });
 
-flashcardRouter.put('/:suuid', async (req, res) => {
+flashcardRouter.put("/:suuid", async (req, res) => {
 	const suuid = req.params.suuid;
 	const newFlashcard: INewFlashcard = req.body;
-	const replacedFlashcard = await flashcardHandler.replaceFlashcard(suuid, newFlashcard);
+	const replacedFlashcard = await flashcardHandler.replaceFlashcard(
+		suuid,
+		newFlashcard
+	);
 	if (replacedFlashcard) {
 		res.json(replacedFlashcard);
 	} else {
 		res.status(404).json({
-			message: `Flashcard with suuid "${suuid}" was not found.`
-		})
+			message: `Flashcard with suuid "${suuid}" was not found.`,
+		});
 	}
 });
 
-flashcardRouter.patch('/:suuid', async (req, res) => {
+flashcardRouter.patch("/:suuid", async (req, res) => {
 	const suuid = req.params.suuid;
 	const patchFlashcard: IPatchFlashcard = req.body;
-	const patchedFlashcard = await flashcardHandler.replaceSomeFieldsInFlashcard(suuid, patchFlashcard);
+	const patchedFlashcard =
+		await flashcardHandler.replaceSomeFieldsInFlashcard(
+			suuid,
+			patchFlashcard
+		);
 	if (patchedFlashcard) {
 		res.json(patchedFlashcard);
 	} else {
 		res.status(404).json({
-			message: `Flashcard with suuid "${suuid}" was not found.`
-		})
+			message: `Flashcard with suuid "${suuid}" was not found.`,
+		});
 	}
 });
 
-flashcardRouter.delete('/:suuid', async (req, res) => {
+flashcardRouter.delete("/:suuid", async (req, res) => {
 	const suuid = req.params.suuid;
 	const deletedFlashcard = await flashcardHandler.deleteFlashcard(suuid);
 	if (deletedFlashcard) {
 		res.json(deletedFlashcard);
 	} else {
 		res.status(404).json({
-			message: `Flashcard with suuid "${suuid}" was not found.`
-		})
+			message: `Flashcard with suuid "${suuid}" was not found.`,
+		});
 	}
 });
